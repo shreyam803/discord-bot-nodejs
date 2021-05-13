532fe9814162f94f0a79586e5699f46f20d7c113
Shreya Maheshwari <shreyam803@gmail.com>
2021-05-13 14:16:27 +0530

    added kick command

--- src/bot.js
+++ src/bot.js
@@ -3,12 +3,40 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 const client = new Client();
 
+const PREFIX = '$'
+
 client.on('ready',()=>{
     console.log(`${client.user.tag} has logged in!`)
 });
 
 client.on('message',(message)=>{
-    console.log(message.content)
+    if(message.author.bot) return;
+   // console.log(`[${message.author.tag}] : ${message.content} `);
+    if(message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/)
+    
+        if(CMD_NAME === 'kick'){
+            if(args.length===0) return message.reply('Please provide an ID.');
+            const member = message.guild.members.cache.get(args[0]);
+            if(member) {
+                member
+                .kick()
+                .then((member)=> message.channel.send(`${member} was kicked.`))
+                .catch((error)=> message.channel.send('I do not have permissions :('))
+            }
+            else{
+                message.channel.send('That member was not found!!')
+            }
+
+            console.log(member);
+        }
+        
+        console.log(CMD_NAME);
+        console.log(args);
+    }
 })
 
 client.login(process.env.DISCORD_BOT_TOKEN);
